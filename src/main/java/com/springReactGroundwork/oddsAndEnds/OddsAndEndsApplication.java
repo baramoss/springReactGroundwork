1eec9a578f2e479c01fb8658e4fbdf347944df27
baramoss <baramoss420@gmail.com>
2023-06-07 01:29:42 +0900

    first commit

---
+++ src/main/java/com/springReactGroundwork/oddsAndEnds/OddsAndEndsApplication.java
@@ -0,0 +1,13 @@
+package com.springReactGroundwork.oddsAndEnds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddsAndEndsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OddsAndEndsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springReactGroundwork/oddsAndEnds/OddsAndEndsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springReactGroundwork.oddsAndEnds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OddsAndEndsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
